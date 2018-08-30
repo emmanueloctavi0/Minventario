4764277cc454ef71ba533df538442181193a7080
Emmanuel Octavio Martínez Calderón <emmanueloctaviomc@gmail.com>
2018-08-29 22:59:19 -0500

    initial commit

---
+++ database/migrations/2018_08_30_000923_create_articles_table.php
@@ -0,0 +1,37 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateArticlesTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('articles', function (Blueprint $table) {
+            $table->increments('id');
+            $table->timestamps();
+            $table->string('name',100);
+            $table->text('description');
+            $table->unsignedInteger('amount');
+            $table->unsignedDecimal('price', 8,2);
+            $table->string('image',250)->default('https://broxtechnology.com/images/iconos/boxs.png');
+
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('articles');
+    }
+}
